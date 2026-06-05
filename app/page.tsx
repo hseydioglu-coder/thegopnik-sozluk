703f943e83b898935d98d42388e7dc8373ce570d
Huseyin <hseydioglu@gmail.com>
2026-06-06 02:00:41 +0300

    gercek firebase sifreleri girildi

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,14 @@ import { getFirestore, collection, query, getDocs } from "firebase/firestore";
 
 // Firebase Kurulumu (Vercel Çevre Değişkenlerinden Okuyacak)
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+  apiKey: "AIzaSyBYdaNaMFJ1yheOXuuac-Aadts9RjUjTxc",
+  authDomain: "thegopnik-fb24e.firebaseapp.com",
+  databaseURL: "https://thegopnik-fb24e-default-rtdb.firebaseio.com",
+  projectId: "thegopnik-fb24e",
+  storageBucket: "thegopnik-fb24e.firebasestorage.app",
+  messagingSenderId: "333801156032",
+  appId: "1:333801156032:web:12d3e87fc9d42061ea2c25",
+  measurementId: "G-J6H0S92575"
 };
 
 const app = getApps().length === 0 ? initializeApp(firebaseConfig) : getApps()[0];
